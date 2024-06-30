9c8d1f92237363bd692e65204bd85407e5a9bda0
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 10:46:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+function checkEmptyObj(obj){
+  if(typeof obj !== `object` || obj == null || Array.isArray(obj)){
+  return false
+  }
+  return Object.keys(obj).length == 0
+}
+
+console.log(checkEmptyObj({})); // true
+console.log(checkEmptyObj({ name: "John", age: 30 })); // false
+console.log(checkEmptyObj(null)); // false
+console.log(checkEmptyObj([])); // false
+console.log(checkEmptyObj(123)); // false
